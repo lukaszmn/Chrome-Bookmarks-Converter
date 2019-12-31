7dd494fc6560eec898ae49c2d1b44aaa337e50d3
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2019-12-31 11:34:56 +0100

    Added generic processing

--- src/convert.js
+++ src/convert.js
@@ -1,25 +1,33 @@
 function ready() {
 	document.getElementById('convert').addEventListener('click', () => {
-		const editor = document.getElementById('convertText');
-
-		const text = editor.value;
-
-		const read = id => document.getElementById(id).value
-			.replace('\\t', '\t')
-			.replace('\\n', '\n');
-
-		const config = {
-			indent: read('indent'),
-			indentAfterHeader: document.getElementById('indentAfterHeader').checked,
-			formatHeader: read('formatHeader'),
-			formatUrl: read('formatUrl'),
-			formatFolder: read('formatFolder')
-		};
-
-		const converted = convert(text, config);
-
-		editor.value = converted;
+		try {
+			processAll();
+		} catch (err) {
+			alert(err);
+		}
 	});
+}
+
+function processAll() {
+	const editor = document.getElementById('convertText');
+
+	const text = editor.value;
+
+	const read = id => document.getElementById(id).value
+		.replace('\\t', '\t')
+		.replace('\\n', '\n');
+
+	const config = {
+		indent: read('indent'),
+		indentAfterHeader: document.getElementById('indentAfterHeader').checked,
+		formatHeader: read('formatHeader'),
+		formatUrl: read('formatUrl'),
+		formatFolder: read('formatFolder')
+	};
+
+	const converted = convert(text, config);
+
+	editor.value = converted;
 }
 
 
@@ -27,13 +35,15 @@ document.addEventListener('DOMContentLoaded', ready, false);
 
 
 const SEP = '&^]';
+const TYPE_URL = 'url';
+const TYPE_FOLDER = 'folder';
 
 
 function convert(text, config) {
 	const obj = JSON.parse(text);
 
 	let res = '';
-	for (let rootName in obj.roots) {
+	for (const rootName in obj.roots) {
 		// rootName == "bookmark_bar"
 		const root = obj.roots[rootName];
 
@@ -48,39 +58,71 @@ function convert(text, config) {
 		res += name + '\n';
 
 		const firstIndent = config.indentAfterHeader ? config.indent : '';
-		res += process(root, [], firstIndent, config);
+		const parsed = parse(root, []);
+		res += format(parsed, firstIndent, config).join('\n');
 	}
 
 	return res;
 }
 
 
-function process(parent, parentNames, indent, config) {
-	let res = '';
+function parse(parent, parentNames) {
+	const res = [];
 
-	for (let child of parent.children) {
+	for (const child of parent.children) {
 		if (child.url) {
-			const formatted = config.formatUrl
-				.replace('NAME', SEP + 'NAME' + SEP)
-				.replace('URL', SEP + 'URL' + SEP)
-				.replace(SEP + 'NAME' + SEP, child.name)
-				.replace(SEP + 'URL' + SEP, child.url);
-			res += indent + formatted + '\n';
+			res.push({
+				type: TYPE_URL,
+				name: child.name,
+				url: child.url
+			});
 		} else if (child.children) {
 			const parentNames2 = parentNames.slice(0);
 			parentNames2.push(child.name);
 			const parentNamesS = parentNames2.join(' > ');
 
-			const formatted = config.formatFolder
-				.replace('NAME', SEP + 'NAME' + SEP)
-				.replace('PATH', SEP + 'PATH' + SEP)
-				.replace(SEP + 'NAME' + SEP, child.name)
-				.replace(SEP + 'PATH' + SEP, parentNamesS);
-			res += indent + formatted + '\n';
-			
-			res += process(child, parentNames2, indent + config.indent, config);
+			const children = parse(child, parentNames2);
+
+			res.push({
+				type: TYPE_FOLDER,
+				name: child.name,
+				path: parentNamesS,
+				children: children
+			});
 		} else {
 			console.log('Invalid node:', child);
+		}
+	}
+
+	return res;
+}
+
+
+function format(arr, indent, config) {
+	const res = [];
+
+	for (const item of arr) {
+		switch (item.type) {
+
+			case TYPE_URL:
+				const formattedUrl = config.formatUrl
+					.replace('NAME', SEP + 'NAME' + SEP)
+					.replace('URL', SEP + 'URL' + SEP)
+					.replace(SEP + 'NAME' + SEP, item.name)
+					.replace(SEP + 'URL' + SEP, item.url);
+				res.push(indent + formattedUrl);
+				break;
+
+			case TYPE_FOLDER:
+				const formattedFolder = config.formatFolder
+					.replace('NAME', SEP + 'NAME' + SEP)
+					.replace('PATH', SEP + 'PATH' + SEP)
+					.replace(SEP + 'NAME' + SEP, item.name)
+					.replace(SEP + 'PATH' + SEP, item.path);
+				res.push(indent + formattedFolder);
+
+				res.push(...format(item.children, indent + config.indent, config));
+				break;
 		}
 	}
 
